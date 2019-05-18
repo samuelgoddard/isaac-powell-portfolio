57f44613b1e4eb93b8b820e92514b337829fdf6d
Sam Goddard <hello@samgoddard.co.uk>
2019-05-18 14:17:02 +0100

    Site Footer component added to default layout, global spacing added

---
+++ components/SiteFooter.vue
@@ -0,0 +1,44 @@
+<template>
+  <footer class="w-full flex flex-wrap items-end">
+    <div class="w-full md:flex-1 mb-4 md:mb-0">
+      <span class="text-7 md:text-8 lg:text-9 xl:text-11 2xl:text-14 tracking-widest uppercase block">Things I do</span>
+      <span class="text-15 md:text-14 lg:text-base xl:text-20 2xl:text-26">Interactive Design, Visual Identity, Design Consultancy</span>
+    </div>
+
+    <div class="w-full md:flex-1 md:ml-auto md:text-right">
+      <a 
+        v-for="(social, index) in socials"
+        class="
+          inline-block
+          underline
+          mx-4 
+          text-15 md:text-14 lg:text-base xl:text-20 2xl:text-26"
+        :class="[{'md:mr-0': index === socials.length - 1}, {'ml-0 md:ml-3' : index === socials.length - socials.length }]"
+        :key="social.name"
+        :href="social.uri"
+        target="_blank"
+        rel="noopener">
+          {{ social.name }}
+      </a>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      socials: [{
+        name: 'Twitter',
+        uri: 'https://twitter.com/BavidDyrne'
+      },{
+        name: 'Dribbble',
+        uri: 'https://dribbble.com/IsaacPowell'
+      }, {
+        name: 'Instagram',
+        uri: 'https://www.instagram.com/ijpowell/'
+      }]
+    }
+  }
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>
+  <div class="
+    h-full min-h-screen
+    flex flex-col
+    p-8 md:p-12 lg:p-20 xl:p-24 2xl:p-32"
+  >
     <nuxt />
+    <site-footer />
   </div>
 </template>
+
+<script>
+import SiteFooter from '~/components/SiteFooter.vue'
+
+export default {
+  components: {
+    SiteFooter
+  }
+}
+</script>
