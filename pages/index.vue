338a095a1ffcee657a1fb4f79016bc46a48e7586
Sam Goddard <hello@samgoddard.co.uk>
2019-05-18 14:17:26 +0100

    scaffolding home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,15 @@
 <template>
-  <section class="h-screen flex items-center justify-center">
+  <section class="flex-1 flex items-center justify-center">
     <!-- <logo /> -->
-    <div class="w-4/5 md:w-2/3 lg:w-1/2">
-      <h1 class="text-21 md:text-31 lg:text-32 xl:text-40 2xl:text-52 font-serif font-medium text-center">I’m Isaac, a freelance designer from Nottingham, UK. <nuxt-link to="/projects">See my work</nuxt-link>, or <a href="mailto:isaac@40000ft.co.uk">drop me a line</a> if you want to work with me on your next project.</h1>
+    <div class="w-4/5 md:w-3/5 lg:w-1/2 xl:w-2/5">
+      <h1 class="
+        leading-snug tracking-tight
+        text-21 md:text-31 lg:text-32 xl:text-42 2xl:text-52
+        font-serif
+        text-center"
+      >
+        I’m Isaac, a freelance designer from Nottingham, UK. <nuxt-link to="/projects" class="underline">See my work</nuxt-link>, or <a href="mailto:isaac@40000ft.co.uk" class="underline">drop me a line</a> if you want to work with me on your next project.
+      </h1>
     </div>
   </section>
 </template>
