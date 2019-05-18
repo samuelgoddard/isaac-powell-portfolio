5041ca6bdc8c5fddb2ef3da770fc7e1b9ef7bfb7
Sam Goddard <hello@samgoddard.co.uk>
2019-05-18 15:05:29 +0100

    seperator component created

---
+++ components/Seperator.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    class="block border-b border-black"
+    :class="width"
+  ></div>
+</template>
+
+<script>
+export default {
+  props: {
+    width: {
+      type: String,
+      required: false
+    }
+  }
+}
+</script>
