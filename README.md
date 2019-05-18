f9434147784ecd7dcc9ec2a6f11268c6ee870dc3
Sam Goddard <hello@samgoddard.co.uk>
2019-05-18 13:17:49 +0100

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# Isaac Powell Portfolio
+
+> Nuxt and Tailwind 1.0 powered portfolio site for Isaac Powell
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn run dev
+
+# build for production and launch server
+$ yarn run build
+$ yarn start
+
+# generate static project
+$ yarn run generate
+```
+
+For detailed explanation on how things work, checkout [Nuxt.js docs](https://nuxtjs.org).
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="h-screen flex items-center justify-center">
+    <!-- <logo /> -->
+    <div class="w-4/5 md:w-2/3 lg:w-1/2">
+      <h1 class="text-21 md:text-31 lg:text-32 xl:text-40 2xl:text-52 font-serif font-medium text-center">I’m Isaac, a freelance designer from Nottingham, UK. <nuxt-link to="/projects">See my work</nuxt-link>, or <a href="mailto:isaac@40000ft.co.uk">drop me a line</a> if you want to work with me on your next project.</h1>
+    </div>
+  </section>
+</template>
+
+<script>
+// import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    // Logo
+  }
+}
+</script>
