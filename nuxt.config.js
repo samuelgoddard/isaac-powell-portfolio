5c524ae184594460bd73445c1185df68def20cd6
Sam Goddard <hello@samgoddard.co.uk>
2019-05-19 23:38:40 +0100

    lang tag added to hhtml tag for a11y

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,6 +9,9 @@ module.exports = {
   */
   head: {
     title: 'Isaac Powell',
+    htmlAttrs: {
+      lang: 'en',
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
