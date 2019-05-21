f2bf8fb4d98e97a72ad3bbf64f0c490111240016
Sam Goddard <hello@samgoddard.co.uk>
2019-05-21 23:48:18 +0100

    added baffle.js dependency, added baffle animation to projects nav overview for feedback

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,7 +1,24 @@
 <template>
   <nuxt-link 
     to="/"
-    class="font-serif leading-snug tracking-tight text-28 md:text-42 lg:text-43 xl:text-47 2xl:text-63 align-middle inline-block no-underline">
+    class="font-serif leading-snug tracking-tight text-28 md:text-42 lg:text-43 xl:text-47 2xl:text-63 align-middle inline-block no-underline logo w-24">
       IJP
     </nuxt-link>
 </template>
+
+<script>
+import baffle from 'baffle';
+
+export default {
+  methods: {
+    // startBaffle () {
+    //   const b = baffle('.logo').start();
+
+    //   b.start()
+    //   .set({ speed: 75 })
+    //   .text(text => 'IJP')
+    //   .reveal(750);
+    // },
+  }
+}
+</script>
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,47 +1,49 @@
 <template>
   <section class="flex-1 flex items-center justify-center">
-      <div class="flex flex-wrap w-full">
-        <div class="w-full md:ml-auto md:w-1/2 overflow-hidden">
-          <parallax-container class="overflow-hidden">
-          <parallax-element :parallaxStrength="15" :type="'depth'">
-          <nav class="w-full overflow-y-auto overflow-x-hidden max-h-64 lg:max-h-90 xl:max-h-128 block hide-scrollbars md:p-8 opacity-0 scale-85" ref="menu" @mouseover="isHovering = true" @mouseout="isHovering = false">
-            <ul>
-              <li 
-                v-for="(project, index) in projects"
-                :key="project.name"
-                ref="numbers"
-                class="mb-8 lg:mb-12 flex flex-wrap items-center"
-              >
-                <nuxt-link
-                  @mouseover.native="selected = project.id"
-                  @mouseout.native="selected = undefined"
-                  ref="opaque"
-                  class="
-                    flex flex-wrap items-center lg:items-end
-                    leading-none tracking-tight
-                    font-serif
-                    text-white
-                    mb-2 lg:mb-0
-                    text-32 md:text-43 lg:text-48 xl:text-52 h-trim
-                    transition"
-                  :class="[{'opacity-25' : isHovering}, { 'opacity-100' :project.id == selected}]"
-                  :to="project.uri">
-                  <span class="font-serif text-8 uppercase w-6 lg:mb-4">{{ project.id }}</span>
-                    {{ project.name }}
-                  <span class="w-full text-6 lg:w-auto lg:text-7 ml-6 lg:ml-3 lg:mb-1 mt-2 lg:mt-0 tracking-widest uppercase block">{{ project.date }} / {{ project.meta }}</span>
-                </nuxt-link>
-              </li>
-            </ul>
-          </nav>
-          </parallax-element>
-          </parallax-container>
-        </div>
+    <div class="flex flex-wrap w-full">
+      <div class="w-full md:ml-auto md:w-1/2 overflow-hidden">
+        <parallax-container class="overflow-hidden">
+        <parallax-element :parallaxStrength="15" :type="'depth'">
+        <nav class="w-full overflow-y-auto overflow-x-hidden max-h-64 lg:max-h-90 xl:max-h-128 block hide-scrollbars md:p-8 opacity-0 scale-85" ref="menu" @mouseover="isHovering = true" @mouseout="isHovering = false">
+          <ul>
+            <li 
+              v-for="(project, index) in projects"
+              :key="project.name"
+              ref="numbers"
+              class="mb-8 lg:mb-12 flex flex-wrap items-center"
+            >
+              <nuxt-link
+                @mouseover.native="startBaffle(project.id, project.name)"
+                @focus.native="startBaffle(project.id, project.name)"
+                @mouseout.native="selected = undefined"
+                ref="opaque"
+                class="
+                  flex flex-wrap items-center lg:items-end
+                  leading-none tracking-tight
+                  font-serif
+                  text-white
+                  mb-2 lg:mb-0
+                  text-32 md:text-43 lg:text-48 xl:text-52 h-trim
+                  transition"
+                :class="[{ 'opacity-25' : isHovering }, { 'opacity-100' :project.id == selected }]"
+                :to="project.uri">
+                <span class="font-serif text-8 uppercase w-6 lg:mb-4">{{ project.id }}</span>
+                  <span :class="project.id">{{ project.name }}</span>
+                <span class="w-full text-6 lg:w-auto lg:text-7 ml-6 lg:ml-3 lg:mb-1 mt-2 lg:mt-0 tracking-widest uppercase block">{{ project.date }} / {{ project.meta }}</span>
+              </nuxt-link>
+            </li>
+          </ul>
+        </nav>
+        </parallax-element>
+        </parallax-container>
+      </div>
     </div>
   </section>
 </template>
 
 <script>
-import { TweenMax } from 'gsap';
+import { TweenMax } from "gsap";
+import baffle from "baffle";
 
 export default {
   transition: 'fade',
@@ -91,6 +93,15 @@ export default {
     }
   },
   methods : {
+    startBaffle (id, name) {
+      this.selected = id
+
+      const b = baffle('.' + id).start();
+      b.start()
+      .set({ speed: 75 })
+      .text(text => name)
+      .reveal(750);
+    },
   },
   mounted () {
     TweenMax.to(this.$refs.menu, 0.3, { css: { scale: 1, autoAlpha:1 }, delay: 0.25 })
