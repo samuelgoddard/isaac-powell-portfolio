3b032f3c4bab99e66ef7e35768b977ca8b6dacaf
Sam Goddard <hello@samgoddard.co.uk>
2019-05-21 23:09:41 +0100

    home page hover gif loop working, animation changed to snap in

--- pages/index.vue
+++ pages/index.vue
@@ -6,23 +6,12 @@
     <div class="w-full md:w-4/5 lg:w-3/5 xl:w-2/5 md:mr-auto md:-ml-20 relative">
 
       <div class="relative flex items-end justify-end">
+        
         <div class="z-0 absolute right-0 top-0 bottom-0 right-0 bg-cover bg-center imageBox opacity-0 transition" ref="imageBox">
         </div>
         
         <mq-layout mq="md+">
-          <div class="z-0 absolute right-0 top-0 bottom-0 right-0 bg-cover bg-center imageBox imageBoxCarousel" ref="imageBoxCarousel">
-            <no-ssr placeholder="Loading...">
-              <agile :infinite="true" :autoplay="true" :fade="true" :autoplaySpeed="650" :arrows="false" :dots="false">
-                <div 
-                  v-for="(image, index) in images"
-                  :key="image.image"
-                  class="slide imageBoxCarouselInner"
-                  :class="`imageBoxCarouselInner-${ index }`"
-                  :style="`background-image: url('${ image.image }')`">
-                </div>
-              </agile>
-            </no-ssr>
-          </div>
+          <img src="/images/portfolio-loop.gif" alt="Portfolio" class="w-90 z-50 opacity-0 fixed" ref="imageBoxCarousel" />
         </mq-layout>
 
         <div ref="heading" class="opacity-0 scale-50">
@@ -37,7 +26,7 @@
                     text-center
                     py-16 md:p-16 lg:p-24"
                   >
-                    <span ref="opaque">I’m Isaac, a freelance designer from Nottingham, UK.</span> <span ref="work"><nuxt-link v-on:mouseover.native="hover" v-on:mouseout.native="mouseOut" to="/projects" class="underline">See my work</nuxt-link></span><span ref="opaque2">, or </span><a v-on:mouseover="hoverLine" v-on:mouseout="mouseOutLine" ref="line" href="mailto:isaac@40000ft.co.uk" class="underline">drop me a line</a><span ref="opaque3"> if you want to work with me on your next project.</span>
+                    <span ref="opaque">I’m Isaac, a freelance designer from Nottingham, UK.</span> <span ref="work"><nuxt-link v-on:mouseover.native="hover" v-on:mousemove.native="mouseMove" v-on:mouseout.native="mouseOut" to="/projects" class="underline">See my work</nuxt-link></span><span ref="opaque2">, or </span><a v-on:mouseover="hoverLine" v-on:mouseout="mouseOutLine" ref="line" href="mailto:isaac@40000ft.co.uk" class="underline">drop me a line</a><span ref="opaque3"> if you want to work with me on your next project.</span>
                   </h1>
                 </mq-layout>
                 <mq-layout mq="sm">
@@ -56,16 +45,6 @@
             </parallax-container>
         </div>
       </div>
-
-
-      <!-- <div class="absolute top-0 right-0 z-0" id="image-pin">
-        <div v-if="showImages">
-            <img :src="otherImage" />
-        </div>
-        <div v-else>
-          <img :src="image"  />
-        </div>
-      </div> -->
     </div>
   </section>
 </template>
@@ -82,33 +61,26 @@ export default {
   data() {
     return { 
       showImages: false,
-      image: "/images/isaac.jpg",
-      images: [
-        { image: "/images/cpmg.jpg" },
-        { image: "/images/hive.jpg" },
-        { image: "/images/misc.jpg" },
-        { image: "/images/paul-smith.jpg" },
-        { image: "/images/yale.jpg" }
-      ]
+      image: "/images/isaac.jpg"
     }
   },
   methods : {
-    hover : function() {
-      TweenMax.to(this.$refs.imageBox, 0.3, { css: { scale: 1, autoAlpha: 0 } })
-      TweenMax.to(this.$refs.imageBoxCarousel, 0.3, { css: { scale: 0.75, autoAlpha: 0.15 } })
+    hover : function(e) {
+      TweenMax.to(this.$refs.imageBoxCarousel, 0, { css: { autoAlpha: 1, left: e.pageX + 30, top: e.pageY + 30 } })
       TweenMax.to(this.$refs.opaque, 0.3, { css: { autoAlpha:0.15 } })
       TweenMax.to(this.$refs.opaque2, 0.3, { css: { autoAlpha:0.15 } })
       TweenMax.to(this.$refs.opaque3, 0.3, { css: { autoAlpha:0.15 } })
       TweenMax.to(this.$refs.line, 0.3, { css: { autoAlpha:0.15 } })
     },
+    mouseMove : function(e) {
+      TweenMax.to(this.$refs.imageBoxCarousel, 0, { css: { left: e.pageX + 30, top: e.pageY + 30 } })
+    },
     mouseOut : function() {
-      TweenMax.killChildTweensOf(this.$refs.imageBoxCarousel)
-      TweenMax.to(this.$refs.imageBox, 0.3, { css: { scale: 1, autoAlpha: 1 } })
       TweenMax.to(this.$refs.opaque, 0.3, { css: { autoAlpha: 1 } })
       TweenMax.to(this.$refs.opaque2, 0.3, { css: { autoAlpha: 1 } })
       TweenMax.to(this.$refs.opaque3, 0.3, { css: { autoAlpha: 1 } })
       TweenMax.to(this.$refs.line, 0.3, { css: { autoAlpha: 1 } })
-      TweenMax.to(this.$refs.imageBoxCarousel, 0.3, { css: { scale: 1, autoAlpha: 0 } })
+      TweenMax.to(this.$refs.imageBoxCarousel, 0, { css: { scale: 1, autoAlpha: 0 } })
     },
     hoverLine : function() {
       TweenMax.to(this.$refs.opaque, 0.3, { css: { autoAlpha:0.15 } })
