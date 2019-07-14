8cf1559d12d76d71872951b49cb27e70517c691f
Sam Goddard <hello@samgoddard.co.uk>
2019-07-14 10:57:25 +0100

    fixed safari logo crop

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,7 +1,7 @@
 <template>
   <nuxt-link
     to="/"
-    class="font-serif leading-tight tracking-tight text-32 md:text-42 lg:text-43 xl:text-47 align-middle inline-block no-underline logo">
+    class="font-serif leading-tight tracking-tight text-32 md:text-40 xl:text-47 align-middle inline-block no-underline logo">
       IJP
     </nuxt-link>
 </template>
