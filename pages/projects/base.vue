a0bbfa47b0b72a76e447e720bc5ddb89ef91986c
Sam Goddard <hello@samgoddard.co.uk>
2019-07-14 12:02:29 +0100

    base animations fixed

--- pages/projects/base.vue
+++ pages/projects/base.vue
@@ -21,7 +21,7 @@ export default {
       tl.staggerTo(document.querySelectorAll('.top-mask'), 3, { y: -100, autoAlpha: 0, rotation: -5, delay: -1, force3D: true, ease: Power4.easeInOut }, -0.17);
       tl.staggerTo(document.querySelectorAll('.bottom-mask'), 3, { y: 100, autoAlpha: 0, rotation: 5, delay: -1, force3D: true, ease: Power4.easeInOut }, -0.17);
       tl.staggerTo(document.querySelectorAll('.seperator-reveal-staggered'), 1.5, { scaleX: 0, autoAlpha: 1, transformOrigin:"left center", delay: -1, ease: Power4.easeInOut }, -0.25);
-      tl.to(document.querySelectorAll('.content'), 3, { y: 0, autoAlpha: 0, delay: -0.65, rotation: -0, force3D: true, ease: Power4.easeInOut });
+      tl.to(document.querySelectorAll('.content'), 4, { y: 450, autoAlpha: 0, delay: -0.65, rotation: -20, force3D: true, ease: Power4.easeInOut });
       tl.to(document.querySelectorAll('.meta'), 2, { y: 25, autoAlpha: 1, delay: -1, rotation: -5, force3D: true, ease: Power4.easeInOut });
       tl.to(document.querySelector('.seperator-reveal'), 2.65, { scaleX: 0, autoAlpha: 1, transformOrigin:"left center", delay: -0.5, ease: Power4.easeInOut, onComplete: done });
       tl.to(document.querySelectorAll('.bleed--all'), 3.5, { autoAlpha: 0, delay: -0.5, rotation: 0, force3D: true, ease: Power4.easeInOut });
